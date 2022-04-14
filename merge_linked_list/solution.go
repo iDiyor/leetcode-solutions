a874e43ce789d632ce4e022024ed4f2fc1084312
Diyor <diyor.islomov@gmail.com>
2022-04-14 13:25:38 +0500

    next portion of soltions

--- merge_linked_list/solution.go
+++ merge_linked_list/solution.go
@@ -1,1 +1,85 @@
 package mergelinkedlist
+
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+
+Constraints:
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func newListNode(val int) *ListNode {
+	return &ListNode{
+		Val: val,
+	}
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	newHead := newListNode(-1)
+	currPtr := newHead
+
+	ptr1 := list1
+	ptr2 := list2
+
+	for ptr1 != nil && ptr2 != nil {
+		if ptr1.Val == ptr2.Val {
+			currPtr.Next = newListNode(ptr1.Val)
+			currPtr = currPtr.Next
+			currPtr.Next = newListNode(ptr2.Val)
+			ptr1 = ptr1.Next
+			ptr2 = ptr2.Next
+		} else if ptr1.Val < ptr2.Val {
+			currPtr.Next = newListNode(ptr1.Val)
+			ptr1 = ptr1.Next
+		} else {
+			currPtr.Next = newListNode(ptr2.Val)
+			ptr2 = ptr2.Next
+		}
+
+		currPtr = currPtr.Next
+	}
+
+	for ptr1 != nil {
+		currPtr.Next = newListNode(ptr1.Val)
+		currPtr = currPtr.Next
+		ptr1 = ptr1.Next
+	}
+
+	for ptr2 != nil {
+		currPtr.Next = newListNode(ptr2.Val)
+		currPtr = currPtr.Next
+		ptr2 = ptr2.Next
+	}
+
+	return newHead.Next
+}
--- merge_linked_list/solution_test.go
+++ merge_linked_list/solution_test.go
@@ -1,1 +1,99 @@
 package mergelinkedlist
+
+import (
+	"fmt"
+	"testing"
+)
+
+func createListFromArray(arr []int) *ListNode {
+	if arr == nil {
+		return nil
+	}
+
+	var head *ListNode
+	var curr *ListNode
+	for _, val := range arr {
+		if head == nil {
+			head = &ListNode{
+				Val:  val,
+				Next: nil,
+			}
+			curr = head
+		} else {
+			curr.Next = &ListNode{
+				Val: val,
+			}
+			curr = curr.Next
+		}
+	}
+	return head
+}
+
+func (n *ListNode) Print() {
+	fmt.Printf("\n####### START #######\n")
+	result := "["
+
+	curr := n
+
+	for curr != nil {
+		result += fmt.Sprintf("%d,", curr.Val)
+		curr = curr.Next
+	}
+
+	result += "]"
+
+	fmt.Printf("list -> %s\n", result)
+	fmt.Printf("####### END #######")
+}
+
+func TestMergeTwoLists(t *testing.T) {
+	type test struct {
+		list1  *ListNode
+		list2  *ListNode
+		output *ListNode
+	}
+
+	tests := []test{
+		{
+			list1:  createListFromArray([]int{1, 2, 4}),
+			list2:  createListFromArray([]int{1, 3, 4}),
+			output: createListFromArray([]int{1, 1, 2, 3, 4, 4}),
+		},
+		{
+			list1:  nil,
+			list2:  nil,
+			output: nil,
+		},
+		{
+			list1:  nil,
+			list2:  createListFromArray([]int{0}),
+			output: createListFromArray([]int{0}),
+		},
+	}
+
+	for idx, test := range tests {
+		result := mergeTwoLists(test.list1, test.list2)
+		if !isEqual(result, test.output) {
+			t.Errorf("wrong result for test #%d", idx)
+		}
+	}
+}
+
+func isEqual(list1 *ListNode, list2 *ListNode) bool {
+	if list1 == nil && list2 == nil {
+		return true
+	}
+
+	curr1 := list1
+	curr2 := list2
+
+	for curr1 != nil && curr2 != nil {
+		if curr1.Val != curr2.Val {
+			return false
+		}
+		curr1 = curr1.Next
+		curr2 = curr2.Next
+	}
+
+	return true
+}
